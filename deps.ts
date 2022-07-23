ec41cc0df44afb3a8095e12efaa70acdd2aa88f0
Masanori Tani <metro@uta8a.org>
2022-07-24 07:10:52 +0900

    feat: get entry detail from Toggl API
    Fix #2

---
+++ deps.ts
@@ -0,0 +1,7 @@
+import dayjs from "https://cdn.skypack.dev/dayjs";
+import utc from "https://cdn.skypack.dev/dayjs/plugin/utc";
+import timezone from "https://cdn.skypack.dev/dayjs/plugin/timezone";
+import duration from "https://cdn.skypack.dev/dayjs/plugin/duration";
+import relativeTime from "https://cdn.skypack.dev/dayjs/plugin/relativeTime";
+
+export { dayjs, duration, relativeTime, timezone, utc };
